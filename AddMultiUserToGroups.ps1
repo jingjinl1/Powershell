96de2bf9c9d66bc6abba9167305318539df1ec18
jingjinl1 <jingjinli266@gmail.com>
2024-05-21 13:02:04 +1000

    multi user and multi groups

---
+++ AddMultiUserToGroups.ps1
@@ -0,0 +1,25 @@
+# Variables
+$users = @("User1", "User2", "User3")
+$groups = @(
+    "Group1", 
+    "Group2", 
+    "Group3")
+
+# Add users to each group
+foreach ($user in $users) {
+    foreach ($group in $groups) {
+        try {
+            # Check if the user is already a member of the group
+            $isMember = Get-ADGroupMember -Identity $group -Recursive | Where-Object { $_.SamAccountName -eq $user }
+
+            if ($isMember) {
+                Write-Output "$user is already in $group."
+            } else {
+                Add-ADGroupMember -Identity $group -Members $user -ErrorAction Stop
+                Write-Output "Added $user to $group successfully."
+            }
+        } catch {
+            Write-Error "Failed to add $user to $group. Error: $_"
+        }
+    }
+}
---
+++ RemoveMultiUserFromGroups.ps1
@@ -0,0 +1,25 @@
+# Variables
+$users = @("User1", "User2", "User3")
+$groups = @(
+    "Group1", 
+    "Group2", 
+    "Group3")
+
+# Remove users from each group
+foreach ($user in $users) {
+    foreach ($group in $groups) {
+        try {
+            # Check if the user is a member of the group
+            $isMember = Get-ADGroupMember -Identity $group -Recursive | Where-Object { $_.SamAccountName -eq $user }
+
+            if ($isMember) {
+                Remove-ADGroupMember -Identity $group -Members $user -Confirm:$false -ErrorAction Stop
+                Write-Output "$user successfully removed from $group."
+            } else {
+                Write-Output "$user is not a member of $group."
+            }
+        } catch {
+            Write-Error "Failed to remove $user from $group. Error: $_"
+        }
+    }
+}
