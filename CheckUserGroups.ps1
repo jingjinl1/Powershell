be46bac9c6ec9404b112cc7f0f3c6c87f9a16198
jingjinl1 <jingjinli266@gmail.com>
2024-05-23 10:54:00 +1000

    updates

--- CheckUserGroups.ps1
+++ CheckUserGroups.ps1
@@ -2,10 +2,10 @@
 $userName = "XXX"
 
 # Retrieve user's groups that contain "XXXX" in the name
-$groupsContainingXXXX = Get-ADUser -Identity $userName -Properties MemberOf |
+$groupsContaining = Get-ADUser -Identity $userName -Properties MemberOf |
     Select-Object -ExpandProperty MemberOf |
     Get-ADGroup |
     Where-Object { $_.Name -like "*XXXX*" }
 
 # Output the groups
-$groupsContainingXXXX | Format-Table Name, DistinguishedName
+$groupsContaining | Format-Table Name, DistinguishedName
