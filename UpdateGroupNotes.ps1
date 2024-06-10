37b0bff21be98e7f7079cc2b957026b3b302bb4a
jingjinl1 <jingjinli266@gmail.com>
2024-06-10 17:40:10 +1000

    update group notes for multiple groups

--- UpdateGroupNotes.ps1
+++ UpdateGroupNotes.ps1
@@ -4,3 +4,21 @@ $newNotes = "Primary Owner: XXX `r`nSecondary Owner: XXX"
 
 # Update the group's notes
 Set-ADGroup -Identity $groupName -Replace @{info=$newNotes}
+
+
+
+
+# Update multiple group notes to the same one
+$groupNames = @("Group1", 
+                "Group2", 
+                "Group3")  
+
+# Define the new notes
+$newNotes = "Primary Owner: XXX `r`nSecondary Owner: XXX"
+
+# Loop through each group and update the notes
+foreach ($groupName in $groupNames) {
+    Set-ADGroup -Identity $groupName -Replace @{info=$newNotes}
+    Write-Output "Updated notes for group: $groupName"
+}
+
