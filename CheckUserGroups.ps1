51c90a3bbd35021c11ff64174b0da5a2ef8a2bcb
jingjinl1 <jingjinli266@gmail.com>
2024-06-11 13:30:39 +1000

    display notes when checking user groups

--- CheckUserGroups.ps1
+++ CheckUserGroups.ps1
@@ -4,8 +4,7 @@ $userName = "XXX"
 # Retrieve user's groups that contain "XXXX" in the name
 $groupsContaining = Get-ADUser -Identity $userName -Properties MemberOf |
     Select-Object -ExpandProperty MemberOf |
-    Get-ADGroup |
-    Where-Object { $_.Name -like "*XXXX*" }
+    Get-ADGroup | Where-Object { $_.Name -like "*XXXX*" }
 
 # Retrieve the notes for each group and output the results
 $groupsContaining | Get-ADGroup -Property info | Select-Object Name, info | Format-Table -AutoSize -Wrap
