74b4c181491c90cbbf2b023c5f79f2f1dce1889a
jingjinl1 <jingjinli266@gmail.com>
2024-06-11 17:18:06 +1000

    improved update notes function

--- UpdateGroupNotes.ps1
+++ UpdateGroupNotes.ps1
@@ -2,8 +2,19 @@
 $groupName = "XXXXX"
 $newNotes = "Primary Owner: XXX `r`nSecondary Owner: XXX"
 
-# Update the group's notes
-Set-ADGroup -Identity $groupName -Replace @{info=$newNotes}
+try {
+    # Update the group's notes
+    Set-ADGroup -Identity $groupName -Replace @{info=$newNotes} 
+    Write-Output "`nUpdated notes for group: $groupName"
+
+    # Get and display the group's updated notes
+    $groupInfo = Get-ADGroup -Identity $groupName -Properties Description, info | Select-Object Name, info
+    $groupInfo | Format-Table -AutoSize -Wrap
+}
+catch {
+    Write-Error "Failed to update notes for group: $groupName"
+}
+
 
 
 
@@ -18,7 +29,16 @@ $newNotes = "Primary Owner: XXX `r`nSecondary Owner: XXX"
 
 # Loop through each group and update the notes
 foreach ($groupName in $groupNames) {
-    Set-ADGroup -Identity $groupName -Replace @{info=$newNotes}
-    Write-Output "Updated notes for group: $groupName"
+    try {
+        Set-ADGroup -Identity $groupName -Replace @{info=$newNotes}
+        Write-Output "Updated notes for group: $groupName"
+        
+        $groupInfo = Get-ADGroup -Identity $groupName -Properties Description,info | Select-Object Name, info
+        $groupInfo | Format-Table -AutoSize -Wrap
+    }
+    catch {
+        Write-Error "Failed to update notes for group: $groupName"
+    }
 }
+
 
