12137c8632e24acd766559e7739c933352928134
jingjinl1 <jingjinli266@gmail.com>
2024-07-04 16:38:23 +1000

    updates

--- AddOrRemoveMember.ps1
+++ AddOrRemoveMember.ps1
@@ -3,11 +3,11 @@ $GroupName = "XXXX"
 $UserName = "XXXX"
 
 if(Get-ADGroupMember -Identity $GroupName | Where-Object { $_.SamAccountName -eq $UserName }) {
-    Write-Output "User $UserName is already a member of the group $GroupName."
+    Write-Output "User $UserName is already a member of the group $GroupName"
 } else {
     # If not, add the user to the group
     Add-ADGroupMember -Identity $GroupName -Members $UserName
-    Write-Output "User $UserName has been added to the group $GroupName."
+    Write-Output "User $UserName has been added to the group $GroupName"
 }
 
 
@@ -21,7 +21,7 @@ $UserName = "XXXX"
 if (Get-ADGroupMember -Identity $GroupName | Where-Object { $_.SamAccountName -eq $UserName }) {
     # If the user is a member, remove them from the group
     Remove-ADGroupMember -Identity $GroupName -Members $UserName -Confirm:$false
-    Write-Output "User $UserName has been removed from the group $GroupName."
+    Write-Output "User $UserName has been removed from the group $GroupName"
 } else {
-    Write-Output "User $UserName is not a member of the group $GroupName."
+    Write-Output "User $UserName is not a member of the group $GroupName"
 }
